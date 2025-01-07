56f98aa0dc919a65f30c7dff05175af6cced6959
Carlo <48788661+CarloFalco@users.noreply.github.com>
2025-01-07 22:13:23 +0100

    Fist Commit with new main code

---
+++ Code/lib/ESP32GithubOtaUpdate/ESP32GithubOtaUpdate.h
@@ -0,0 +1,213 @@
+#pragma once
+
+#ifdef ENABLE_ESP32_GITHUB_OTA_UPDATE_DEBUG
+  #ifdef DEBUG_ESP_PORT
+    #define DEBUG_ESP32GOA(...) DEBUG_ESP_PORT.printf( __VA_ARGS__ )
+  #else
+    #define DEBUG_ESP32GOA(...) if(Serial) Serial.printf( __VA_ARGS__ )
+  #endif
+#else
+  #define DEBUG_ESP32GOA(x...) if (false) do { (void)0; } while (0)
+#endif
+extern bool needToStayAlive;
+
+class ESP32GithubOtaUpdate {
+  public:
+    void setOTADownloadUrl(const char* otaDownloadUrl);
+    void setVersionCheckUrl(const char* versionCheckUrl);
+    void setCurrentFirmwareVersion(int currentFirmwareVersion);
+    void setUpdateCheckInterval(int updateCheckInterval);
+    void begin();    
+    void checkForOTA();
+          
+  private:
+    void setClock();    
+    bool doVersionCheck();
+    void doFirmwareUpdate();
+
+    const char * leCaCrt = \
+      "-----BEGIN CERTIFICATE-----\n" \
+      "MIIEyDCCA7CgAwIBAgIQDPW9BitWAvR6uFAsI8zwZjANBgkqhkiG9w0BAQsFADBh\n" \
+      "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+      "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n" \
+      "MjAeFw0yMTAzMzAwMDAwMDBaFw0zMTAzMjkyMzU5NTlaMFkxCzAJBgNVBAYTAlVT\n" \
+      "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxMzAxBgNVBAMTKkRpZ2lDZXJ0IEdsb2Jh\n" \
+      "bCBHMiBUTFMgUlNBIFNIQTI1NiAyMDIwIENBMTCCASIwDQYJKoZIhvcNAQEBBQAD\n" \
+      "ggEPADCCAQoCggEBAMz3EGJPprtjb+2QUlbFbSd7ehJWivH0+dbn4Y+9lavyYEEV\n" \
+      "cNsSAPonCrVXOFt9slGTcZUOakGUWzUb+nv6u8W+JDD+Vu/E832X4xT1FE3LpxDy\n" \
+      "FuqrIvAxIhFhaZAmunjZlx/jfWardUSVc8is/+9dCopZQ+GssjoP80j812s3wWPc\n" \
+      "3kbW20X+fSP9kOhRBx5Ro1/tSUZUfyyIxfQTnJcVPAPooTncaQwywa8WV0yUR0J8\n" \
+      "osicfebUTVSvQpmowQTCd5zWSOTOEeAqgJnwQ3DPP3Zr0UxJqyRewg2C/Uaoq2yT\n" \
+      "zGJSQnWS+Jr6Xl6ysGHlHx+5fwmY6D36g39HaaECAwEAAaOCAYIwggF+MBIGA1Ud\n" \
+      "EwEB/wQIMAYBAf8CAQAwHQYDVR0OBBYEFHSFgMBmx9833s+9KTeqAx2+7c0XMB8G\n" \
+      "A1UdIwQYMBaAFE4iVCAYlebjbuYP+vq5Eu0GF485MA4GA1UdDwEB/wQEAwIBhjAd\n" \
+      "BgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwdgYIKwYBBQUHAQEEajBoMCQG\n" \
+      "CCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wQAYIKwYBBQUHMAKG\n" \
+      "NGh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RH\n" \
+      "Mi5jcnQwQgYDVR0fBDswOTA3oDWgM4YxaHR0cDovL2NybDMuZGlnaWNlcnQuY29t\n" \
+      "L0RpZ2lDZXJ0R2xvYmFsUm9vdEcyLmNybDA9BgNVHSAENjA0MAsGCWCGSAGG/WwC\n" \
+      "ATAHBgVngQwBATAIBgZngQwBAgEwCAYGZ4EMAQICMAgGBmeBDAECAzANBgkqhkiG\n" \
+      "9w0BAQsFAAOCAQEAkPFwyyiXaZd8dP3A+iZ7U6utzWX9upwGnIrXWkOH7U1MVl+t\n" \
+      "wcW1BSAuWdH/SvWgKtiwla3JLko716f2b4gp/DA/JIS7w7d7kwcsr4drdjPtAFVS\n" \
+      "slme5LnQ89/nD/7d+MS5EHKBCQRfz5eeLjJ1js+aWNJXMX43AYGyZm0pGrFmCW3R\n" \
+      "bpD0ufovARTFXFZkAdl9h6g4U5+LXUZtXMYnhIHUfoyMo5tS58aI7Dd8KvvwVVo4\n" \
+      "chDYABPPTHPbqjc1qCmBaZx2vN4Ye5DUys/vZwP9BFohFrH/6j/f3IL16/RZkiMN\n" \
+      "JCqVJUzKoZHm1Lesh3Sz8W2jmdv51b2EQJ8HmA==\n" \
+      "-----END CERTIFICATE-----";
+
+
+  const char* _otaDownloadUrl; 
+  const char* _versionCheckUrl;
+  int _currentFirmwareVersion;
+  int _updateCheckInterval = 60; // 60 seconds.   
+};
+
+void ESP32GithubOtaUpdate::setUpdateCheckInterval(int updateCheckInterval) {
+    _updateCheckInterval = updateCheckInterval;
+    Serial.print("updateCheckInterval: ");
+    Serial.println("Done");
+}
+
+void ESP32GithubOtaUpdate::setOTADownloadUrl(const char* otaDownloadUrl) {
+    _otaDownloadUrl = otaDownloadUrl;
+    Serial.print("otaDownloadUrl: ");
+    Serial.println("Done");
+}
+
+void ESP32GithubOtaUpdate::setVersionCheckUrl(const char* versionCheckUrl) {
+    _versionCheckUrl = versionCheckUrl;
+    Serial.print("versionCheckUrl: ");
+    Serial.print(versionCheckUrl);
+    Serial.println("\t Done");
+}
+
+void ESP32GithubOtaUpdate::setCurrentFirmwareVersion(int currentFirmwareVersion) {
+    _currentFirmwareVersion = currentFirmwareVersion;
+    Serial.print("currentFirmwareVersion: ");
+    Serial.println("Done");
+}
+
+void ESP32GithubOtaUpdate::doFirmwareUpdate() {
+  DEBUG_ESP32GOA("[doFirmwareUpdate()]: Downloading new firmware update ..\r\n");
+
+  WiFiClientSecure client;
+  client.setCACert(leCaCrt);
+  client.setTimeout(12);
+  client.setHandshakeTimeout(8);
+   
+  {  // WiFiClientSecure needs to be destroyed after HttpReleaseUpdate
+    HttpReleaseUpdate httpUpdate;
+    httpUpdate.rebootOnUpdate(true);
+
+    httpUpdate.onStart([](void) {
+      needToStayAlive = 1;
+      DEBUG_ESP32GOA("[doFirmwareUpdate()]: Start downloading..\r\n");
+    });
+    httpUpdate.onEnd([](bool success) {
+      DEBUG_ESP32GOA("[doFirmwareUpdate()]: Downloading ended.\r\n");
+      needToStayAlive = 0;
+      //if (success) SPIFFS.remove("/update");
+    });
+    httpUpdate.onProgress([](int progress, int total) {
+      DEBUG_ESP32GOA("[doFirmwareUpdate()]: Downloading: %d\r\n", (progress / (total / 100)));
+    });
+    auto ret = httpUpdate.update(client, _otaDownloadUrl);
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        DEBUG_ESP32GOA("[doFirmwareUpdate()]: Http Update Failed (Error=%d): %s\r\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+        break;
+      case HTTP_UPDATE_NO_UPDATES:
+        DEBUG_ESP32GOA("[doFirmwareUpdate()]: No Update!\r\n");
+        break;
+      case HTTP_UPDATE_OK:
+        DEBUG_ESP32GOA("[doFirmwareUpdate()]: Update OK!\r\n");
+        break;
+    }
+  }
+}
+
+bool ESP32GithubOtaUpdate::doVersionCheck() { 
+  DEBUG_ESP32GOA("[doVersionCheck()]: Checking for new firmware update ..\r\n");
+
+  Serial.println("UpdateCheck-> checking");
+  bool isUpdateAvailable = false;
+  WiFiClientSecure *client = new WiFiClientSecure;
+
+  if(client) {
+    client->setCACert(leCaCrt);
+    client->setTimeout(12);
+    {
+       // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+      HTTPClient https;
+      if (https.begin(*client, _versionCheckUrl)) {
+        int httpCode = https.GET();  
+
+        if (httpCode > 0) {
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            String server_fw_version = https.getString();     
+            int new_fw_version_int = server_fw_version.toInt();            
+            DEBUG_ESP32GOA("[doVersionCheck()]: Current FW version: %d. Server FW version: %d \r\n", _currentFirmwareVersion, new_fw_version_int);
+            
+            if (_currentFirmwareVersion == new_fw_version_int) {
+              DEBUG_ESP32GOA("[doVersionCheck()]: Device is already on latest firmware version: %d\r\n", _currentFirmwareVersion);                            
+            } else {
+              DEBUG_ESP32GOA("[doVersionCheck()]: New firmware version available version!\r\n");
+              isUpdateAvailable = true;
+            }
+          }
+        } else {
+          DEBUG_ESP32GOA("[doVersionCheck()]: GET... failed, error: %s\r\n", https.errorToString(httpCode).c_str());
+        }  
+        https.end();
+      } else {
+        DEBUG_ESP32GOA("[doVersionCheck()]: Unable to connect\r\n");
+      }      
+    } // End extra scoping block  
+    delete client;
+  } else {
+    DEBUG_ESP32GOA("[doVersionCheck()]: Unable to create WiFiClientSecure client!!\r\n");
+  } 
+
+  return isUpdateAvailable;
+}
+
+void ESP32GithubOtaUpdate::checkForOTA()
+{
+    for (;;)
+    {
+      if(doVersionCheck()) {
+        Serial.println("UpdateCheck");
+        doFirmwareUpdate();
+      }
+      vTaskDelay((_updateCheckInterval * 1000) / portTICK_PERIOD_MS);
+    }
+}
+
+void ESP32GithubOtaUpdate::setClock() {
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");  // UTC
+  DEBUG_ESP32GOA("Waiting for NTP time sync: ");
+
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    yield();
+    delay(500);
+    DEBUG_ESP32GOA(".");
+    now = time(nullptr);
+  }
+  DEBUG_ESP32GOA("\r\n");
+
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  DEBUG_ESP32GOA("Current time: %s\r\n", asctime(&timeinfo));
+}
+
+void ESP32GithubOtaUpdate::begin() {
+  // setClock();
+
+  xTaskCreate([](void* o){ static_cast<ESP32GithubOtaUpdate*>(o)->checkForOTA(); },
+          "ESP32OTAUpdateTask",    // Name of the task (for debugging)
+          1024 * 8,                // Stack size (bytes)
+          this,                   // Parameter to pass
+          5,                      // Task priority
+          NULL);                    // Task handle
+}
--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,158 +1,74 @@
-/*********
-  Complete project details at https://randomnerdtutorials.com  
-*********/
+#define ENABLE_ESP32_GITHUB_OTA_UPDATE_DEBUG Uncomment to enable logs.
 
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include <DHT.h>
-
+#include <Arduino.h>
+#include <WiFiClientSecure.h>
+#include "HttpReleaseUpdate.h"
+#include "ESP32GithubOtaUpdate.h"
+#include "secret.h"
 
 
-/*#include <SPI.h>
-#define BME_SCK 14
-#define BME_MISO 12
-#define BME_MOSI 13
-#define BME_CS 15*/
+const char* OTA_FILE_LOCATION = "https://raw.githubusercontent.com/CarloFalco/ArduinoTest/refs/heads/Mqtt/11_OTA/ESP_OTA/firmware.bin";
+const char* VERSION_URL = "https://raw.githubusercontent.com/CarloFalco/ArduinoTest/refs/heads/Mqtt/11_OTA/ESP_OTA/version.txt";
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-void printValues(void);
+const int current_fw_version = 2024121101;  // YYYYMMDDRR where R = release of the day
 
-Adafruit_BME280 bme; // I2C
-unsigned long delayTime;
-float BME_Temperature; // Temperature
-float BME_Humidity; // Humidity
-float BME_Pressure; // Pressure
-float BME_Altitude; // Altitude
+ESP32GithubOtaUpdate otaUpdate;
+bool needToStayAlive = 0;
+#define LED_BUILTIN 97
 
+bool ledState = false; // Variabile per lo stato del LED
 
-#define DHTPIN 19   //Pin a cui è connesso il sensore
-#define DHTTYPE DHT22   //Tipo di sensore che stiamo utilizzando (DHT22)
-DHT dht(DHTPIN, DHTTYPE); //Inizializza oggetto chiamato "dht", parametri: pin a cui è connesso il sensore, tipo di dht 11/22
-//Variabili
-float DHT_Temperature; // Temperature
-float DHT_Humidity; // Humidity
+void setupWiFi() {
+  #if defined(ESP8266)
+    WiFi.setSleepMode(WIFI_NONE_SLEEP); 
+    WiFi.setAutoReconnect(true);
+    Serial.print("ESP8266");
+  #elif defined(ESP32)
+    WiFi.setSleep(false); 
+    WiFi.setAutoReconnect(true);
+    Serial.print("ESP32");
+  #endif
 
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.println("");
 
-//Include the library
-#include <MQUnifiedsensor.h>
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-//Definitions
-#define placa "ESP 32"
-#define Voltage_Resolution 3.3 // 3.3
-#define pin A0 //Analog input 0 of your arduino
-#define type "MQ-135" //MQ135
-#define ADC_Bit_Resolution 12 // For arduino UNO/MEGA/NANO 12
-#define RatioMQ135CleanAir 3.6//RS / R0 = 3.6 ppm  
+}
 
-// Creo l'oggetto
-MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
-  /*
-    Exponential regression:
-  GAS      | a      | b
-  CO       | 605.18 | -3.937  
-  Alcohol  | 77.255 | -3.18 
-  CO2      | 110.47 | -2.862
-  Toluen  | 44.947 | -3.445
-  NH4      | 102.2  | -2.473
-  Aceton  | 34.668 | -3.369
-  */
-
+void setupOtaUpdate() {
+  otaUpdate.setOTADownloadUrl(OTA_FILE_LOCATION);
+  otaUpdate.setVersionCheckUrl(VERSION_URL);
+  otaUpdate.setCurrentFirmwareVersion(current_fw_version);
+  otaUpdate.setUpdateCheckInterval(60); // Check every 60 seconds.
+  otaUpdate.begin();
+}
 
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println(F("BME280 test"));
-
-    bool status;
-
-    // configurazione del BME280
-    // (you can also pass in a Wire library object like &Wire2)
-    status = bme.begin(0x76);  
-    if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-    }
-
-    Serial.println("-- Default Test --");
-    delayTime = 1000;
-
-    //configurazione del DHT22
-    dht.begin();
-    Serial.println();
-
-    //configurazione del DHT22  
-    MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
-    MQ135.setA(110.47); MQ135.setB(-2.862); // Configure the equation to to calculate NH4 concentration
-    MQ135.init(); 
-
-    Serial.print("Calibrating please wait.");
-    float calcR0 = 0;
-    for(int i = 1; i<=10; i ++)
-    {
-    MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
-    calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
-    Serial.print(".");
-    }
-    MQ135.setR0(calcR0/10);
-    Serial.println("  done!.");
-
-    if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
-    if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
-    /*****************************  MQ CAlibration ********************************************/ 
-    //MQ135.serialDebug(true);
-
+  Serial.begin(115200);
+  Serial.print("WelcomeToNewBuild");
+  setupWiFi();   
+  pinMode(LED_BUILTIN, OUTPUT);
+  setupOtaUpdate();
 }
 
 
-void loop() { 
-    printValues();
-    MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
-    MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-    //MQ135.serialDebug(); // Will print the table on the serial port
-    Serial.println(MQ135.readSensor()); // Serial
-    delay(delayTime);
 
-}
+void loop(){
 
-
-void printValues() {
-
-    BME_Temperature = bme.readTemperature();
-    BME_Humidity = bme.readHumidity();
-    BME_Pressure = bme.readPressure() / 100.0F;
-    BME_Altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
-    Serial.println("--------------------");
-    Serial.println("BME280");
-    Serial.print("Temperature = ");
-    Serial.print(BME_Temperature);
-    Serial.println(" *C");
-
-    Serial.print("Pressure = ");
-    Serial.print(BME_Pressure);
-    Serial.println(" hPa");
-
-    Serial.print("Approx. Altitude = ");
-    Serial.print(BME_Altitude);
-    Serial.println(" m");
-
-    Serial.print("Humidity = ");
-    Serial.print(BME_Humidity);
-    Serial.println(" %");
-
-    Serial.println();
-
-    if (!isnan(dht.readHumidity())) {
-        DHT_Humidity = dht.readHumidity();
-        DHT_Temperature= dht.readTemperature();
-    }
-    Serial.println("--------------------");
-    Serial.println("DHT22");
-    Serial.print("Umidità: ");
-    Serial.print(DHT_Humidity);
-    Serial.print(" %, Temp: ");
-    Serial.print(DHT_Temperature);
-    Serial.println(" Celsius");
-
+  ledState = !ledState; // Inverti lo stato
+  digitalWrite(LED_BUILTIN, ledState); // Imposta il nuovo stato
+  Serial.print("needToStayAlive ST: ");
+  delay(1000);
 }
